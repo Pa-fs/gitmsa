a6ea225c4d241605fce3b43eaa1f17f0db476535
Pa-fs <qordi124@gmail.com>
2025-01-13 21:19:48 +0900

    리액트 네이티브 실습 기초

---
+++ js_work/250110/index.js
@@ -0,0 +1,10 @@
+// console.log(process.env)
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+// require('dotenv').config()
+
+// console.log(process.env.NODE_ENV)
+
+console.log(process.env.NODE_ENV)
